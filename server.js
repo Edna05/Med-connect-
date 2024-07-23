55906e833b611c822723a3f1caa5bc48aa51b12c
ChefOnanga <nangbiteghe@gmail.com>
2024-07-23 16:16:19 +0100

    ajouter med-connect

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
+/*Middleware pour parser les json */
+app.use(express.json());
+
+/*Middleware pour parser les donnees de formulaire */
+
+app.use(express.urlencoded({ extended: false }));
+/*Route */
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`le serveur a bien démarré ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
